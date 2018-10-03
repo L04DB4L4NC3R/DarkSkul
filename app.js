f81308d349d1b63e189e31ec5a30262b893b245a
angad <angadsharma1016@gmail.com>
2018-10-03 17:40:37 +0530

    dialogflow webhook

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const hbs = require("express-hbs");
 const path = require("path");
+const bp = require("body-parser");
+
+app.use(bp.json());
+app.use(bp.urlencoded({extended:false}));
 
 // handlebars
 app.engine('hbs',hbs.express4());
@@ -11,6 +15,10 @@ app.use(express.static("static"));
 
 app.get("/",(req,res)=>{
     res.render("index",{name:"angad"});
+});
+
+app.post("/chatbot",(req,res)=>{
+    res.json(req.body)
 });
 
 
